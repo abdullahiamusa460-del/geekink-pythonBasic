a053ff3185f4ed2114941403268b52cdbcd2a0da
abdullahi <abdullahiamusa460@gmail.com>
2026-05-16 12:31:17 +0100

    feat: python variable input output

---
+++ index.py
@@ -0,0 +1,12 @@
+# This is a simple Python program that prints "Hello, World!" to the console.
+
+print("Hello, World!")
+
+# input from the user
+name = input("ENTER YOUR NAME: ")
+print("Hello, " + name + "! Welcome to the world of Python programming!") 
+
+age = int(input("ENTER YOUR AGE: "))
+print("You are " + str(age) + " years old.")
+print(type(name))
+print(type(age))
